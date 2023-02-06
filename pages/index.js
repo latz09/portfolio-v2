b824e534e4bf8ef45ecfbacb01da04c4db6d5f06
latz09 <85132328+latz09@users.noreply.github.com>
2023-02-06 13:12:40 -0600

    removed gifs

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,13 @@ import Landing from '@/components/Landing';
 import GifDisplay from '@/components/utils/GifDisplay';
 import Icons from '@/components/utils/Icons';
 import Image from 'next/image';
-import gif from '../public/images/gifs/movie-connection.gif'
-
+import gif from '../public/images/gifs/movie-connection.gif';
 
 export default function Home() {
 	return (
 		<div className=' grid gap-12 my-12 lg:my-24  max-w-7xl mx-4 lg:mx-auto  '>
 			<Landing />
 			<Icons />
-			<div className="my-16 ">
-				<GifDisplay />
-			</div>
-			
 		</div>
 	);
 }
-
-
